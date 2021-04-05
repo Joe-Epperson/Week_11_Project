3a859fc067fd4b28c5e5aa8bbaa218ad7146c47e
Joe Epperson IV <jee4cf@umsystem.edu>
2021-04-05 15:06:51 -0500

    Finished the Program

--- Fraction.cpp
+++ Fraction.cpp
@@ -1,6 +1,6 @@
-// Your name
-// Your UMKC Email Address
-// Today's date
+// Joe Epperson, IV
+// jee4cf@umsystem.edu
+// 4/05/2021
 // CS201L Lab 11
 
 #include "Fraction.h"
@@ -12,18 +12,113 @@ using namespace std;
 // For reducing
 int Fraction::getGCD(int num1, int num2)
 {
-	int remainder = num2 % num1;
-	if (remainder != 0)
-		return getGCD(remainder, num1);
-	return num1;
+	try
+	{
+		if (num1 == 0)
+		{
+			throw runtime_error("Error: Cannot divide by zero");
+		}
+		int remainder = num2 % num1;
+		if (remainder != 0)
+			return getGCD(remainder, num1);
+		return num1;
+	}
+	catch (runtime_error& e)
+	{
+		cout << e.what() << endl;
+	}
 }
 
 // Reduce/simplify a fraction
 void Fraction::reduce()
 {
 	// Alter this function later to adjust for negative values
-	int gcd = getGCD(numerator, denominator);
-	numerator /= gcd;
-	denominator /= gcd;
+	try
+	{
+		int gcd = getGCD(numerator, denominator);
+		if (gcd == 0)
+		{
+			throw runtime_error("Error: Cannot divide by zero");
+		}
+		numerator /= gcd;
+		denominator /= gcd;
+	}
+	catch (runtime_error& e)
+	{
+		cout << e.what() << endl;
+	}
+
+	// Changing the values based on whether or not they are negative
+	if ((numerator < 0) && (denominator < 0))
+	{
+		numerator *= -1;
+		denominator *= -1;
+	}
+	else if ((denominator < 0) && (numerator >= 0))
+	{
+		numerator *= -1;
+		denominator *= -1;
+	}
 }
 
+// Defining the operator overload functions for '+' '-' '*' '/'
+const Fraction Fraction::operator+(Fraction rhs)
+{
+	Fraction endVal;
+	endVal.denominator = denominator * rhs.denominator;
+	endVal.numerator = (numerator * (endVal.denominator / denominator)) + (rhs.numerator * (endVal.denominator / rhs.denominator));
+	endVal.reduce();
+	return endVal;
+}
+
+const Fraction Fraction::operator-(Fraction rhs)
+{
+	Fraction endVal;
+	endVal.denominator = denominator * rhs.denominator;
+	endVal.numerator = (numerator * (endVal.denominator / denominator)) - (rhs.numerator * (endVal.denominator / rhs.denominator));
+	endVal.reduce();
+	return endVal;
+}
+
+const Fraction Fraction::operator*(Fraction rhs)
+{
+	Fraction endVal;
+	endVal.denominator = denominator * rhs.denominator;
+	endVal.numerator = numerator * rhs.numerator;
+	endVal.reduce();
+	return endVal;
+}
+
+const Fraction Fraction::operator/(Fraction rhs)
+{
+	Fraction endVal;
+	endVal.numerator = numerator * rhs.denominator;
+	endVal.denominator = denominator * rhs.numerator;
+	endVal.reduce();
+	return endVal;
+}
+
+
+// Defining the bool operator overload
+bool Fraction::operator==(Fraction rhs)
+{
+	if ((numerator == rhs.numerator) && (denominator == rhs.denominator))
+	{
+		return true;
+	}
+	return false;
+}
+
+// Defining the operator overload functions for the input stream and the output stream
+ostream& operator<<(ostream& output, const Fraction& fract)
+{
+	output << fract.numerator << '/' << fract.denominator << endl;
+	return output;
+}
+
+istream& operator>>(istream& input, Fraction& fract)
+{
+	char slash;
+	input >> fract.numerator >> slash >> fract.denominator;
+	return input;
+}
--- Fraction.h
+++ Fraction.h
@@ -1,7 +1,7 @@
 #pragma once
-// Your Name
-// Today's date
-// Lab 9
+// Joe Epperson, IV
+// 4/05/2021
+// Lab 11
 
 #include <iostream>
 using namespace std;
@@ -15,10 +15,28 @@ class Fraction
 private:
 	int getGCD(int num1, int num2);
 
+	// Declaring private class variables
+	int numerator;
+	int denominator;
+
 public:
 	void reduce();
 
+	// Declaring and defining a constructor for the Fraction class
+	Fraction() { numerator = 0; denominator = 0; }
 
+	// Declaring the overload operators for input and output streams
+	friend ostream& operator << (ostream& output, const Fraction& fract);
+	friend istream& operator >> (istream& input, Fraction& fract);
+
+	// Declaring the overload operators for '+' '-' '*' '/'
+	const Fraction operator+ (Fraction rhs);
+	const Fraction operator- (Fraction rhs);
+	const Fraction operator* (Fraction rhs);
+	const Fraction operator/ (Fraction rhs);
+
+	// Declaring the overload operator for the equality bool
+	bool operator== (Fraction rhs);
 
 };
 
